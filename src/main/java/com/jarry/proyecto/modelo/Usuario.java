7d36b461abf8f4fb0b07f2b035c14191ce3427f5
Jose <jriverar1@unicartagena.edu.co>
2026-05-25 13:55:15 -0500

    feat: implementacion de vistas y modificacion de algunas clases

--- src/main/java/com/jarry/proyecto/modelo/Usuario.java
+++ src/main/java/com/jarry/proyecto/modelo/Usuario.java
@@ -7,34 +7,27 @@ import jakarta.persistence.*;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto_increment en SQL
     private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
 
     @Column(nullable = false)
     private String clave;
 
-    @Column(nullable = false, length = 100)
-    private String nombre;
-
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false)
     private String rol;
 
+    // CONSTRUCTORES, GETTERS Y SETTERS
     public Usuario() {}
 
-    public Usuario(Long id, String clave, String nombre, String rol) {
-        this.id = id;
-        this.clave = clave;
-        this.nombre = nombre;
-        this.rol = rol;
-    }
-
-    // Getters y Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-    public String getClave() { return clave; }
-    public void setClave(String clave) { this.clave = clave; }
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getClave() { return clave; }
+    public void setClave(String clave) { this.clave = clave; }
     public String getRol() { return rol; }
     public void setRol(String rol) { this.rol = rol; }
 }

e8a419bf90dbe5d483d0bfac75374cc6fb85dba0
Jose <jriverar1@unicartagena.edu.co>
2026-05-25 12:37:22 -0500

    feat: creacion de repositorios JPA para Usuario y Censo

---
+++ src/main/java/com/jarry/proyecto/repositorio/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.jarry.proyecto.repositorio;
+
+import com.jarry.proyecto.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
